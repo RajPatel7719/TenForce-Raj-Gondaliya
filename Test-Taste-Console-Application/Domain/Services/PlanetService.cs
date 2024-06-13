737c242d3fecefaec5ac4747f37e637baeca6f7b
Raj7719 <tbs.rajg@gmail.com>
2024-06-13 13:35:28 +0530

    inform user about the exicution status and add try catch block

--- Test-Taste-Console-Application/Domain/Services/PlanetService.cs
+++ Test-Taste-Console-Application/Domain/Services/PlanetService.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
 using System.Collections.ObjectModel;
 using System.Globalization;
 using System.Text;
@@ -26,72 +27,89 @@ namespace Test_Taste_Console_Application.Domain.Services
         // Method to get all planets with their moons
         public IEnumerable<Planet> GetAllPlanets()
         {
+            Console.WriteLine("Loading data...");
+
             // Initialize an empty collection to store planets
             var allPlanetsWithTheirMoons = new Collection<Planet>();
 
-            // Send a GET request to the API to retrieve all planets with their moons
-            var response = _httpClientService.Client
-                .GetAsync(UriPath.GetAllPlanetsWithMoonsQueryParameters)
-                .Result;
-
-            //If the status code isn't 200-299, then the function returns an empty collection.
-            if (!response.IsSuccessStatusCode)
+            try
             {
-                // Log a warning if the request failed
-                Logger.Instance.Warn($"{LoggerMessage.GetRequestFailed}{response.StatusCode}");
-                return allPlanetsWithTheirMoons;
-            }
+                // Send a GET request to the API to retrieve all planets with their moons
+                var response = _httpClientService.Client
+                    .GetAsync(UriPath.GetAllPlanetsWithMoonsQueryParameters)
+                    .Result;
 
-            // Read the response content as a string
-            var content = response.Content.ReadAsStringAsync().Result;
-
-            //The JSON converter uses DTO's, that can be found in the DataTransferObjects folder, to deserialize the response content.
-            var results = JsonConvert.DeserializeObject<JsonResult<PlanetDto>>(content);
-
-            //The JSON converter can return a null object. 
-            if (results == null) return allPlanetsWithTheirMoons;
-
-            //If the planet doesn't have any moons, then it isn't added to the collection.
-            // Iterate through each planet in the results
-            foreach (var planet in results.Bodies)
-            {
-                // Check if the planet has moons
-                if (planet.Moons != null)
+                //If the status code isn't 200-299, then the function returns an empty collection.
+                if (!response.IsSuccessStatusCode)
                 {
-                    // Initialize a new collection to store the moons
-                    var newMoonsCollection = new Collection<MoonDto>();
-
-                    // Iterate through each moon of the planet
-                    foreach (var moon in planet.Moons)
-                    {
-                        // Send a GET request to the API to retrieve the moon's details
-                        var moonResponse = _httpClientService.Client
-                           .GetAsync(UriPath.GetMoonByIdQueryParameters + moon.URLId)
-                           .Result;
-
-                        // Read the response content as a string
-                        var moonContent = moonResponse.Content.ReadAsStringAsync().Result;
-
-                        // Deserialize the JSON response into a MoonDto object
-                        newMoonsCollection.Add(JsonConvert.DeserializeObject<MoonDto>(moonContent));
-                    }
-
-                    // Assign the new collection of moons to the planet
-                    planet.Moons = newMoonsCollection;
+                    // Log a warning if the request failed
+                    Logger.Instance.Warn($"{LoggerMessage.GetRequestFailed}{response.StatusCode}");
+                    Console.WriteLine("Failed to load data. Status code: " + response.StatusCode);
+                    return allPlanetsWithTheirMoons;
                 }
 
-                // Create a new Planet object from the PlanetDto
-                var newPlanet = new Planet(planet);
+                // Read the response content as a string
+                var content = response.Content.ReadAsStringAsync().Result;
 
-                // Calculate the average moon gravity for the planet
-                newPlanet.CalculateAverageMoonGravity();
+                //The JSON converter uses DTO's, that can be found in the DataTransferObjects folder, to deserialize the response content.
+                var results = JsonConvert.DeserializeObject<JsonResult<PlanetDto>>(content);
 
-                // Add the planet to the collection
-                allPlanetsWithTheirMoons.Add(newPlanet);
+                //The JSON converter can return a null object. 
+                if (results == null)
+                {
+                    Console.WriteLine("There is no planet found.");
+                    return allPlanetsWithTheirMoons;
+                }
+
+                //If the planet doesn't have any moons, then it isn't added to the collection.
+                // Iterate through each planet in the results
+                foreach (var planet in results.Bodies)
+                {
+                    // Check if the planet has moons
+                    if (planet.Moons != null)
+                    {
+                        // Initialize a new collection to store the moons
+                        var newMoonsCollection = new Collection<MoonDto>();
+
+                        // Iterate through each moon of the planet
+                        foreach (var moon in planet.Moons)
+                        {
+                            // Send a GET request to the API to retrieve the moon's details
+                            var moonResponse = _httpClientService.Client
+                               .GetAsync(UriPath.GetMoonByIdQueryParameters + moon.URLId)
+                               .Result;
+
+                            // Read the response content as a string
+                            var moonContent = moonResponse.Content.ReadAsStringAsync().Result;
+
+                            // Deserialize the JSON response into a MoonDto object
+                            newMoonsCollection.Add(JsonConvert.DeserializeObject<MoonDto>(moonContent));
+                        }
+
+                        // Assign the new collection of moons to the planet
+                        planet.Moons = newMoonsCollection;
+                    }
+
+                    // Create a new Planet object from the PlanetDto
+                    var newPlanet = new Planet(planet);
+
+                    // Calculate the average moon gravity for the planet
+                    newPlanet.CalculateAverageMoonGravity();
+
+                    // Add the planet to the collection
+                    allPlanetsWithTheirMoons.Add(newPlanet);
+                }
+
+                Console.WriteLine("Data loaded successfully.");
+                // Return the collection of planets
+                return allPlanetsWithTheirMoons;
             }
-
-            // Return the collection of planets
-            return allPlanetsWithTheirMoons;
+            catch (Exception ex)
+            {
+                Logger.Instance.Error("An error occurred while loading data.", ex);
+                Console.WriteLine("An error occurred while loading data. " + ex.Message);
+                return allPlanetsWithTheirMoons;
+            }
         }
 
         private static string RemoveDiacritics(string text)
